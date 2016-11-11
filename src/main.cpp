5aa61e38abeb6c17d33a1342de8ff21bce0e4c67
Luke Godfrey <thelukester92@me.com>
2016-11-11 12:39:41 -0600

    Implemented efficiency test; it is faster than armadillo.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,31 @@
 #include "tensor.h"
 #include "error.h"
 #include <iostream>
+#include <chrono>
 using namespace nnlib;
 using namespace std;
 
+void testCorrectness();
+double testEfficiency(size_t inps, size_t outs, size_t epochs, function<void()> &start, function<void()> &end);
+
 int main()
+{
+	size_t inps				= 100000;
+	size_t outs				= 1000;
+	size_t epochs			= 10;
+	
+	using clock = chrono::high_resolution_clock;
+	clock::time_point start;
+	function<void()> startFn = [&](void) { start = clock::now(); };
+	function<void()> endFn = [&](void) { cout << "took " << chrono::duration<double>(clock::now() - start).count() / epochs << " seconds per epoch" << endl; };
+	
+	testCorrectness();
+	testEfficiency(inps, outs, epochs, startFn, endFn);
+	
+	return 0;
+}
+
+void testCorrectness()
 {
 	size_t inps = 2, outs = 3;
 	Tensor<double> weights(outs, inps), input(inps), bias(outs), target(outs);
@@ -28,11 +49,30 @@ int main()
 	target(2) = 15.14;
 	
 	Tensor<double> result = weights * input + bias;
-	
 	for(size_t i = 0; i < outs; ++i)
 		Assert(result(i) == target(i), "Linear::forward failed!");
 	
 	cout << "Passed all tests!" << endl;
+}
+
+double testEfficiency(size_t inps, size_t outs, size_t epochs, function<void()> &start, function<void()> &end)
+{
+	Tensor<double> weights(outs, inps);
+	Tensor<double> input(inps), bias(outs), result(outs);
 	
-	return 0;
+	weights.fillNormal();
+	input.fillNormal();
+	bias.fillNormal();
+	result.fill(0.0);
+	
+	start();
+	for(size_t i = 0; i < epochs; ++i)
+		result += weights * input + bias;
+	end();
+	
+	double resultSum = 0.0;
+	for(size_t i = 0; i < outs; ++i)
+		resultSum += result[i];
+	
+	return resultSum;
 }
