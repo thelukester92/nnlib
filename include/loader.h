12ff35a5272c26a09824e32a103e9a3efc8538e6
Luke Godfrey <thelukester92@me.com>
2016-12-18 11:55:37 -0600

    Testing nominal loading.

--- include/loader.h
+++ include/loader.h
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <algorithm>
 #include <cstdlib>
+#include <vector>
+#include <unordered_map>
 #include "matrix.h"
 #include "error.h"
 
@@ -15,11 +17,18 @@ template <typename T>
 class Loader
 {
 public:
+	struct Relation
+	{
+		std::string name;
+		std::vector<std::string> attrNames;
+		std::vector<std::unordered_map<std::string, size_t>> attrVals;
+	};
+	
 	/// Load a weka .arff file. Assumes real values (for now).
-	static Matrix<T> loadArff(const std::string &filename)
+	static Matrix<T> loadArff(const std::string &filename, Relation *relPtr = nullptr)
 	{
 		Vector<Tensor<T> *> rows;
-		Vector<Vector<std::string>> attributes;
+		Relation rel;
 		
 		std::ifstream fin(filename.c_str());
 		NNAssert(fin.is_open(), "Could not open file '" + filename + "'!");
@@ -33,34 +42,42 @@ public:
 			std::transform(line.begin(), line.end(), line.begin(), ::tolower);
 			if(line[0] == '@')
 			{
-				if(line.compare(0, 10, "@attribute") == 0)
+				if(line.compare(0, 9, "@relation") == 0)
+				{
+					char *ptr = const_cast<char *>(line.c_str());
+					skipWhitespace(&ptr);
+					char *end = tokenEnd(ptr);
+					rel.name = std::string(ptr, end - ptr);
+				}
+				else if(line.compare(0, 10, "@attribute") == 0)
 				{
 					char *ptr = const_cast<char *>(line.c_str() + 10);
+					skipWhitespace(&ptr);
+					char *end = tokenEnd(ptr);
+					rel.attrNames.push_back(std::string(ptr, end - ptr));
 					
-					// Skip attribute name
-					ptr = tokenEnd(ptr);
+					ptr = end;
+					skipWhitespace(&ptr);
 					
-					// Check attribute type
-					if(strncmp(ptr, "numeric", 7) == 0 || strncmp(ptr, "integer", 7) == 0 || strncmp(ptr, "real", 4) == 0)
-						attributes.push_back(Vector<std::string>());
-					else if(*ptr == '{')
+					std::unordered_map<std::string, size_t> attrVals;
+					if(*ptr == '{')
 					{
-						Vector<std::string> vals;
+						++ptr;
+						size_t val = 0;
 						while(*ptr != '}' && *ptr != '\0')
 						{
-							char *end = tokenEnd(ptr);
-							vals.push_back(std::string(ptr, end - ptr));
+							char *end = tokenEnd(ptr, ",}");
+							attrVals[std::string(ptr, end - ptr)] = val++;
 							ptr = end;
 							skipWhitespace(&ptr);
 							if(*ptr != '\0' && *ptr != '}')
 								++ptr;
-							
-							std::cout << "found nominal: '" << vals.back() << "'" << std::endl;
 						}
-						attributes.push_back(vals);
 					}
 					else
-						NNAssert(false, "Unrecognized attribute type!");
+						NNAssert(strncmp(ptr, "numeric", 7) == 0 || strncmp(ptr, "integer", 7) == 0 || strncmp(ptr, "real", 4) == 0, "Unrecognized attribute type!");
+					
+					rel.attrVals.push_back(attrVals);
 				}
 				else if(line.compare(0, 5, "@data") == 0)
 					break;
@@ -68,7 +85,7 @@ public:
 		}
 		while(!fin.fail())
 		{
-			Vector<T> *rowPtr = new Vector<T>(attributes.size());
+			Vector<T> *rowPtr = new Vector<T>(rel.attrNames.size());
 			Vector<T> &row = *rowPtr;
 			rows.push_back(rowPtr);
 			
@@ -81,22 +98,35 @@ public:
 			size_t i = 0;
 			while(*ptr != '\0')
 			{
-				while(*ptr == ' ' || *ptr == '\t')
-					++ptr;
+				skipWhitespace(&ptr);
 				if(*ptr == '\0')
 					break;
-				NNAssert(i < attributes.size(), "Too many columns on row " + std::to_string(rows.size()));
-				row[i] = std::strtod(ptr, &ptr);
-				if(*ptr == ',')
-					++ptr;
+				NNAssert(i < rel.attrNames.size(), "Too many columns on row " + std::to_string(rows.size()));
+				if(rel.attrVals[i].size() == 0)
+					row[i] = std::strtod(ptr, &ptr);
+				else
+				{
+					char *end = tokenEnd(ptr, ",");
+					auto j = rel.attrVals[i].find(std::string(ptr, end - ptr));
+					NNAssert(j != rel.attrVals[i].end(), "Invalid nominal value '" + std::string(ptr, end - ptr) + "'");
+					row[i] = j->second;
+					ptr = end - 1;
+				}
+				++ptr;
 				++i;
 			}
-			NNAssert(i == attributes.size(), "Not enough columns on row " + std::to_string(rows.size()));
+			NNAssert(i == rel.attrNames.size(), "Not enough columns on row " + std::to_string(rows.size()));
 		}
 		fin.close();
 		
+		if(relPtr != nullptr)
+			relPtr = new Relation(rel);
+		
 		/// Flatten and return.
-		return Matrix<T>(Vector<T>(rows), rows.size(), attributes.size());
+		Matrix<T> flattened(Vector<T>(rows), rows.size(), rel.attrNames.size());
+		for(auto *i : rows)
+			delete i;
+		return flattened;
 	}
 
 private:
@@ -106,7 +136,7 @@ private:
 			++*ptr;
 	}
 	
-	static char *tokenEnd(char *start)
+	static char *tokenEnd(char *start, const char *delim = " \t")
 	{
 		char *ptr = start;
 		skipWhitespace(&ptr);
@@ -130,7 +160,7 @@ private:
 		}
 		else
 		{
-			while(*ptr != ' ' && *ptr != '\t' && *ptr != '\0')
+			while(strspn(ptr, delim) == 0 && *ptr != '\0')
 				++ptr;
 		}
 		
