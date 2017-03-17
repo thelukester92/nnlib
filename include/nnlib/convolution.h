2e0203b7a1f51d2effd3755370b63eb95b77fab4
Luke Godfrey <thelukester92@me.com>
2017-03-17 12:23:35 -0500

    More work on convolution; need to test, a 7-nested loop is ugly.

--- include/nnlib/convolution.h
+++ include/nnlib/convolution.h
@@ -25,6 +25,8 @@ public:
 	m_inputShape(inCols, inRows, inChannels),
 	m_kernelShape(kernelCols, kernelRows, inChannels),
 	m_outputShape((inCols - kernelCols + 2 * padding) / stride + 1, (inRows - kernelRows + 2 * padding) / stride + 1, kernelCount),
+	m_padding(padding),
+	m_stride(stride),
 	m_kernels(kernelCount, m_kernelShape.size()),
 	m_kernelsBlame(kernelCount, m_kernelShape.size()),
 	m_inputBlame(batch, m_inputShape.size()),
@@ -73,12 +75,41 @@ public:
 		m_outputs.resize(size, m_outputs.cols());
 	}
 	
+	
 	virtual Matrix<T> &forward(const Matrix<T> &inputs) override
 	{
 		NNAssert(inputs.rows() == m_inputBlame.rows(), "Incorrect batch size!");
 		NNAssert(inputs.cols() == m_inputBlame.cols(), "Incorrect input size!");
 		
-		/// \todo convolution
+		for(size_t inp = 0; inp < inputs.rows(); ++inp)
+		{
+			for(size_t ker = 0; ker < m_kernels.rows(); ++ker)
+			{
+				for(size_t outRow = 0; outRow < m_outputShape.height; ++outRow)
+				{
+					int inRow = int(outRow * m_stride) - int(m_padding);
+					for(size_t outCol = 0; outCol < m_outputShape.width; ++outCol)
+					{
+						int inCol = int(outCol * m_stride) - int(m_padding);
+						T &sum = m_outputs(inp, (ker * m_outputShape.height + outRow) * m_outputShape.width + outCol);
+						sum = 0.0;
+						for(size_t channel = 0; channel < m_kernelShape.channels; ++channel)
+						{
+							for(size_t row = 0; row < m_kernelShape.height; ++row)
+							{
+								for(size_t col = 0; col < m_kernelShape.width; ++col)
+								{
+									if(inRow >= 0 && inRow < m_inputShape.height && inCol >= 0 && inCol < m_inputShape.width)
+										sum += m_kernels(ker, (channels * m_kernelShape.height + row) * m_kernelShape.width + col) * inputs(inp, (channel * m_inputShape.height + inRow) * m_inputShape.width + inCol);
+									++inCol;
+								}
+								++inRow;
+							}
+						}
+					}
+				}
+			}
+		}
 		
 		return m_outputs;
 	}
@@ -119,6 +150,8 @@ private:
 	Shape m_inputShape;			///< The width, height, and number of channels in the input.
 	Shape m_kernelShape;		///< The width, height, and number of channels in the kernels.
 	Shape m_outputShape;		///< The width, height, and number of channels in the output.
+	size_t m_padding;			///< How many 0s to pad on each edge.
+	size_t m_stride;			///< How many pixels to skip between steps.
 	Matrix<T> m_kernels;		///< The parameters, imagined as convolutional kernels.
 	Matrix<T> m_kernelsBlame;	///< Gradient of the error w.r.t. the parameters.
 	Matrix<T> m_inputBlame;		///< Gradient of the error w.r.t. the inputs.
