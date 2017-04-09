976d13578a710277cd2e05e18c00f749597a3ec3
Luke Godfrey <thelukester92@me.com>
2017-04-09 14:08:21 -0500

    Misc.

--- include/nnlib/module.h
+++ include/nnlib/module.h
@@ -8,11 +8,43 @@ template <typename T = double>
 class Module
 {
 public:
-	virtual void forward(const Tensor<T> &inputs) = 0;
-	virtual void backward(const Tensor<T> &inputs, const Tensor<T> &blame) = 0;
+	virtual Module &resize(size_t inps, size_t outs)
+	{
+		output().resize(batchSize(), outs);
+		inputBlame().resize(batchSize(), inps);
+	}
 	
-private:
+	virtual Module &resize(size_t inps)
+	{
+		resize(inps, outputs());
+	}
 	
+	virtual Module &batch(size_t bats)
+	{
+		output().resize(bats, outputs());
+		inputBlame().resize(bats, inputs());
+	}
+	
+	virtual Matrix<T> &forward(const Matrix<T> &inputs) = 0;
+	virtual Matrix<T> &backward(const Matrix<T> &inputs, const Matrix<T> &blame) = 0;
+	
+	virtual Matrix<T> &output() = 0;
+	virtual Matrix<T> &inputBlame() = 0;
+	
+	size_t inputs() const
+	{
+		return inputBlame().cols();
+	}
+	
+	size_t outputs() const
+	{
+		return output().cols();
+	}
+	
+	size_t batchSize() const
+	{
+		return output().rows();
+	}
 };
 
 }
