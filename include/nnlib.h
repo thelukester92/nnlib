bcf66127c4fe5986641d3b90498878eb92a9f337
Luke Godfrey <thelukester92@me.com>
2017-04-10 13:18:14 -0500

    Alphabetized nnlib.h

--- include/nnlib.h
+++ include/nnlib.h
@@ -1,31 +1,32 @@
 // Utilities
 #include "nnlib/util/algebra.h"
+#include "nnlib/util/batcher.h"
 #include "nnlib/util/error.h"
 #include "nnlib/util/loader.h"
-#include "nnlib/util/saver.h"
+#include "nnlib/util/matrix.h"
 #include "nnlib/util/progress.h"
 #include "nnlib/util/random.h"
-#include "nnlib/util/batcher.h"
-#include "nnlib/util/timer.h"
+#include "nnlib/util/saver.h"
 #include "nnlib/util/tensor.h"
+#include "nnlib/util/timer.h"
 #include "nnlib/util/vector.h"
-#include "nnlib/util/matrix.h"
+
 
 // Neural Network Modules
-#include "nnlib/modules/container.h"
-#include "nnlib/modules/sequential.h"
+#include "nnlib/modules/activation.h"
 #include "nnlib/modules/concat.h"
-#include "nnlib/modules/linear.h"
+#include "nnlib/modules/container.h"
 #include "nnlib/modules/convolution.h"
 #include "nnlib/modules/identity.h"
-#include "nnlib/modules/activation.h"
+#include "nnlib/modules/linear.h"
+#include "nnlib/modules/sequential.h"
 
 // Activation functions
-#include "nnlib/activations/tanh.h"
 #include "nnlib/activations/logistic.h"
 #include "nnlib/activations/sin.h"
+#include "nnlib/activations/tanh.h"
 
 // Neural Network Utilities
 #include "nnlib/critics/sse.h"
-#include "nnlib/optimizers/sgd.h"
 #include "nnlib/optimizers/rmsprop.h"
+#include "nnlib/optimizers/sgd.h"
