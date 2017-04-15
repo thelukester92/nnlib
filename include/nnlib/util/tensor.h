3b21deaa153377a22d176603bcf7e8db2d5f5ee7
Luke Godfrey <thelukester92@me.com>
2017-04-15 16:32:23 -0500

    Added tests for reshape.

--- include/nnlib/util/tensor.h
+++ include/nnlib/util/tensor.h
@@ -11,12 +11,14 @@ template <typename T>
 class Tensor
 {
 public:
+	/// Create a tensor with the given size and shape.
 	template <typename ... Ts>
 	Tensor(Ts... dims)
 	{
 		resize(dims...);
 	}
 	
+	/// Resize this tensor and, if necessary, its underlying storage.
 	template <typename ... Ts>
 	void resize(Ts... dims)
 	{
@@ -29,16 +31,39 @@ public:
 		m_data.resize(product);
 	}
 	
+	/// Reshape this tensor without changing its size.
+	template <typename ... Ts>
+	void reshape(Ts... dims)
+	{
+		Storage<size_t> newDims = { static_cast<size_t>(dims)... };
+		size_t product = 1;
+		for(size_t i = 0, j = m_dims.size(); i < j; ++i)
+		{
+			product *= newDims[i];
+		}
+		NNAssert(product == m_data.size(), "Incompatible shape!");
+		m_dims = newDims;
+	}
+	
+	/// Get the total number of elements in this tensor.
 	size_t size() const
 	{
 		return m_data.size();
 	}
 	
+	/// Get the size of a given dimension.
 	size_t size(size_t dim) const
 	{
 		return m_dims[dim];
 	}
 	
+	/// Get the number of dimensions in this tensor.
+	size_t dims() const
+	{
+		return m_dims.size();
+	}
+	
+	/// Element access given a multidimensional index.
 	template <typename ... Ts>
 	T &operator()(Ts... indices)
 	{
@@ -48,14 +73,10 @@ private:
 	Storage<size_t> m_dims;
 	Storage<T> m_data;
 	
-	/// a 5x3x2 tensor accessed at 4, 2, 1 should produce:
-	/// 4 * (3*2) + 2 * (2) + 1
-	/// = 1 + 2 * (3 * 4 + 2)
-	/// = (((4 * 3) + 2) * 2) + 1
-	/// = 29
-	
+	/// Get the appropriate contiguous index given the multidimensional index.
 	size_t indexOf(const std::initializer_list<size_t> &indices)
 	{
+		NNAssert(indices.size() == m_dims.size(), "Incorrect number of dimensions!");
 		size_t result = 0, dim = 1;
 		const size_t *i, *j;
 		for(i = indices.begin(), j = indices.end() - 1; i != j; ++i, ++dim)
--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,28 @@ using namespace nnlib;
 
 void testTensor()
 {
-	Tensor<double> tensor(5, 3, 2);
-	NNAssert(tensor.size() == 5*3*2, "Tensor::Tensor(...) yielded the wrong tensor size!");
-	NNAssert(tensor.size(0) == 5, "Tensor::Tensor(...) yielded the wrong 0th dimension size!");
-	NNAssert(tensor.size(1) == 3, "Tensor::Tensor(...) yielded the wrong 1st dimension size!");
-	NNAssert(tensor.size(2) == 2, "Tensor::Tensor(...) yielded the wrong 2nd dimension size!");
+	Tensor<double> tensor(6, 3, 2);
+	NNAssert(tensor.size() == 6*3*2, "Tensor::Tensor yielded the wrong tensor size!");
+	NNAssert(tensor.dims() == 3, "Tensor::Tensor yielded the wrong number of dimensions!");
+	NNAssert(tensor.size(0) == 6, "Tensor::Tensor yielded the wrong 0th dimension size!");
+	NNAssert(tensor.size(1) == 3, "Tensor::Tensor yielded the wrong 1st dimension size!");
+	NNAssert(tensor.size(2) == 2, "Tensor::Tensor yielded the wrong 2nd dimension size!");
 	
+	tensor.reshape(9, 4);
+	NNAssert(tensor.dims() == 2, "Tensor::reshape yielded the wrong number of dimensions!");
+	NNAssert(tensor.size(0) == 9, "Tensor::reshape yielded the wrong 0th dimension size!");
+	NNAssert(tensor.size(1) == 4, "Tensor::reshape yielded the wrong 1st dimension size!");
 	
+	bool causedProblems = false;
+	try
+	{
+		tensor.reshape(3, 3);
+	}
+	catch(const std::runtime_error &e)
+	{
+		causedProblems = true;
+	}
+	NNAssert(causedProblems, "Tensor::reshape failed to yield an error for an incompatible shape!");
 }
 
 void testNeuralNet()
