69025895a3371f7d1db7d8903b99cfc7efaf7e07
Luke Godfrey <thelukester92@me.com>
2017-04-15 22:41:15 -0500

    Better indexOf with strides.

--- include/nnlib/util/tensor.h
+++ include/nnlib/util/tensor.h
@@ -18,17 +18,22 @@ public:
 		resize(dims...);
 	}
 	
+	// MARK: Size and shape methods.
+	
 	/// Resize this tensor and, if necessary, its underlying storage.
 	template <typename ... Ts>
 	void resize(Ts... dims)
 	{
 		m_dims = { static_cast<size_t>(dims)... };
-		size_t product = 1;
-		for(size_t i = 0, j = m_dims.size(); i < j; ++i)
+		m_strides.resize(m_dims.size());
+		
+		m_strides[m_strides.size() - 1] = 1;
+		for(size_t i = m_strides.size() - 1; i > 0; --i)
 		{
-			product *= m_dims[i];
+			m_strides[i - 1] = m_strides[i] * m_dims[i];
 		}
-		m_data.resize(product);
+		
+		m_data.resize(m_strides[0] * m_dims[0]);
 	}
 	
 	/// Reshape this tensor without changing its size.
@@ -36,13 +41,25 @@ public:
 	void reshape(Ts... dims)
 	{
 		Storage<size_t> newDims = { static_cast<size_t>(dims)... };
-		size_t product = 1;
-		for(size_t i = 0, j = m_dims.size(); i < j; ++i)
+		Storage<size_t> newStrides(newDims.size());
+		
+		newStrides[newStrides.size() - 1] = 1;
+		for(size_t i = newStrides.size() - 1; i > 0; --i)
 		{
-			product *= newDims[i];
+			newStrides[i - 1] = newStrides[i] * newDims[i];
 		}
-		NNAssert(product == m_data.size(), "Incompatible shape!");
+		
+		NNAssert(newStrides[0] * newDims[0] == m_data.size(), "Incompatible shape!");
+		m_data.resize(newStrides[0] * newDims[0]);
+		
 		m_dims = newDims;
+		m_strides = newStrides;
+	}
+	
+	/// Get the number of dimensions in this tensor.
+	size_t dims() const
+	{
+		return m_dims.size();
 	}
 	
 	/// Get the total number of elements in this tensor.
@@ -57,11 +74,20 @@ public:
 		return m_dims[dim];
 	}
 	
-	/// Get the number of dimensions in this tensor.
-	size_t dims() const
+	/// Get the stride of a given dimension.
+	size_t stride(size_t dim) const
 	{
-		return m_dims.size();
+		return m_strides[dim];
 	}
+	
+	// MARK: Element manipulation methods.
+	
+	Tensor &fill(const T &value)
+	{
+		
+	}
+	
+	// MARK: Element access methods.
 	
 	/// Element access given a multidimensional index.
 	template <typename ... Ts>
@@ -70,21 +96,19 @@ public:
 		return m_data[indexOf({ static_cast<size_t>(indices)... })];
 	}
 private:
-	Storage<size_t> m_dims;
+	Storage<size_t> m_dims, m_strides;
 	Storage<T> m_data;
 	
 	/// Get the appropriate contiguous index given the multidimensional index.
+	/// \todo BLAS accelerated dot product between indices and m_strides
 	size_t indexOf(const std::initializer_list<size_t> &indices)
 	{
 		NNAssert(indices.size() == m_dims.size(), "Incorrect number of dimensions!");
-		size_t result = 0, dim = 1;
-		const size_t *i, *j;
-		for(i = indices.begin(), j = indices.end() - 1; i != j; ++i, ++dim)
-		{
-			result += *i;
-			result *= m_dims[dim];
-		}
-		return result + *i;
+		size_t sum = 0, idx = 0;
+		for(const size_t *i = indices.begin(), *j = indices.end(); i != j; ++i, ++idx)
+			sum += *i * m_strides[idx];
+		std::cout << sum << std::endl;
+		return sum;
 	}
 };
 
