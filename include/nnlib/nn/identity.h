df4c3bb1e2e7b4140a67a25749a8308c86ab056d
Luke Godfrey <thelukester92@me.com>
2017-05-02 10:17:39 -0500

    Implemented identity activation.

---
+++ include/nnlib/nn/identity.h
@@ -0,0 +1,35 @@
+#ifndef NN_IDENTITY_H
+#define NN_IDENTITY_H
+
+#include <math.h>
+#include "map.h"
+
+namespace nnlib
+{
+
+/// Identity activation function.
+/// Useful in a concat module.
+template <typename T = double>
+class Identity : public Map<T>
+{
+public:
+	using Map<T>::Map;
+	using Map<T>::forward;
+	using Map<T>::backward;
+	
+	/// Single element forward.
+	virtual T forward(const T &x) override
+	{
+		return x;
+	}
+	
+	/// Single element backward.
+	virtual T backward(const T &x, const T &y) override
+	{
+		return 1.0;
+	}
+};
+
+}
+
+#endif
