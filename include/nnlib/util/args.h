c71c214a90eed60539162b93785d852c09d0227a
Luke Godfrey <thelukester92@me.com>
2017-05-11 11:24:27 -0500

    Better args.

--- include/nnlib/util/args.h
+++ include/nnlib/util/args.h
@@ -3,6 +3,9 @@
 
 #include "error.h"
 #include <unordered_map>
+#include <map>
+#include <iostream>
+#include <iomanip>
 
 namespace nnlib
 {
@@ -90,6 +93,15 @@ public:
 	{
 		addOpt(opt, longOpt);
 		m_expected[opt].type = Data::Int;
+		m_data[opt].type = Data::Null;
+		return *this;
+	}
+	
+	ArgsParser &addInt(char opt, std::string longOpt, int defaultValue)
+	{
+		addInt(opt, longOpt);
+		m_data[opt].type = Data::Int;
+		m_data[opt].i = defaultValue;
 		return *this;
 	}
 	
@@ -97,20 +109,36 @@ public:
 	{
 		addOpt(opt, longOpt);
 		m_expected[opt].type = Data::Double;
+		m_data[opt].type = Data::Null;
 		return *this;
 	}
 	
-	ArgsParser &setString(char opt, std::string longOpt = "")
+	ArgsParser &addDouble(char opt, std::string longOpt, double defaultValue)
+	{
+		addDouble(opt, longOpt);
+		m_data[opt].type = Data::Double;
+		m_data[opt].d = defaultValue;
+		return *this;
+	}
+	
+	ArgsParser &addString(char opt, std::string longOpt = "")
 	{
 		addOpt(opt, longOpt);
 		m_expected[opt].type = Data::String;
+		m_data[opt].type = Data::Null;
+		return *this;
+	}
+	
+	ArgsParser &addString(char opt, std::string longOpt, std::string defaultValue)
+	{
+		addString(opt, longOpt);
+		m_data[opt].type = Data::String;
+		m_data[opt].s = defaultValue.c_str();
 		return *this;
 	}
 	
 	ArgsParser &parse(int argc, const char **argv)
 	{
-		m_data.clear();
-		
 		Args args(argc, argv);
 		args.popString();
 		
@@ -123,8 +151,8 @@ public:
 			
 			if(arg.length() > 1 && arg[0] == '-' && arg[1] == '-')
 			{
-				auto i = m_long.find(std::string(arg.c_str() + 2));
-				NNAssert(i != m_long.end(), "Unexpected argument!");
+				auto i = m_longToChar.find(std::string(arg.c_str() + 2));
+				NNAssert(i != m_longToChar.end(), "Unexpected argument!");
 				opt = i->second;
 			}
 			else if(arg.length() > 1 && arg[0] == '-')
@@ -164,48 +192,71 @@ public:
 		return *this;
 	}
 	
+	ArgsParser &printOpts(std::ostream &out = std::cout)
+	{
+		out << std::left;
+		
+		std::map<char, Data> data(m_data.begin(), m_data.end());
+		for(auto &p : data)
+		{
+			out << std::setw(20);
+			auto i = m_charToLong.find(p.first);
+			if(i != m_charToLong.end())
+				out << i->second;
+			else
+				out << p.first;
+			out << "= ";
+			switch(p.second.type)
+			{
+			case Data::Bool:
+				out << p.second.b;
+				break;
+			case Data::Int:
+				out << p.second.i;
+				break;
+			case Data::Double:
+				out << p.second.d;
+				break;
+			case Data::String:
+				out << p.second.s;
+				break;
+			}
+			out << std::endl;
+		}
+		return *this;
+	}
+	
 	bool hasOpt(char opt)
 	{
-		return m_data.find(opt) != m_data.end();
-	}
-	
-	bool hasOpt(std::string longOpt)
-	{
-		auto i = m_long.find(longOpt);
-		return i != m_long.end() && hasOpt(i->second);
-	}
-	
-	int getInt(char opt, int def = 0)
-	{
 		auto i = m_data.find(opt);
-		if(i != m_data.end())
-			return i->second.i;
-		else
-			return def;
+		return i != m_data.end() && i->second.type != Data::Null;
+	}
+	
+	int getInt(char opt)
+	{
+		NNAssert(hasOpt(opt), "Attempted to get undefined option '" + optName(opt) + "'!");
+		NNAssert(m_data.at(opt).type == Data::Int, "Attempted to get an incompatible type!");
+		return m_data.at(opt).i;
 	}
 	
 	double getDouble(char opt, double def = 0)
 	{
-		auto i = m_data.find(opt);
-		if(i != m_data.end())
-			return i->second.d;
-		else
-			return def;
+		NNAssert(hasOpt(opt), "Attempted to get undefined option '" + optName(opt) + "'!");
+		NNAssert(m_data.at(opt).type == Data::Double, "Attempted to get an incompatible type!");
+		return m_data.at(opt).d;
 	}
 	
 	std::string getString(char opt, std::string def = "")
 	{
-		auto i = m_data.find(opt);
-		if(i != m_data.end())
-			return i->second.s;
-		else
-			return def;
+		NNAssert(hasOpt(opt), "Attempted to get undefined option '" + optName(opt) + "'!");
+		NNAssert(m_data.at(opt).type == Data::String, "Attempted to get an incompatible type!");
+		return m_data.at(opt).s;
 	}
 	
 private:
 	struct Data
 	{
-		enum { Bool, Int, Double, String } type;
+		enum { Bool, Int, Double, String, Null } type;
 		union
 		{
 			bool b;
@@ -223,14 +274,25 @@ private:
 		
 		if(longOpt != "")
 		{
-			NNAssert(m_long.find(longOpt) == m_long.end(), "Cannot redefine a long option!");
-			m_long[longOpt] = opt;
+			NNAssert(m_longToChar.find(longOpt) == m_longToChar.end(), "Cannot redefine a long option!");
+			m_longToChar[longOpt] = opt;
+			m_charToLong[opt] = longOpt;
 		}
+	}
+	
+	std::string optName(char opt)
+	{
+		auto i = m_charToLong.find(opt);
+		if(i != m_charToLong.end())
+			return i->second;
+		else
+			return std::string(opt, 1);
 	}
 	
 	std::unordered_map<char, Data> m_expected;
 	std::unordered_map<char, Data> m_data;
-	std::unordered_map<std::string, char> m_long;
+	std::unordered_map<std::string, char> m_longToChar;
+	std::unordered_map<char, std::string> m_charToLong;
 };
 
 }
