39119c42ffd7b4186129c01db03db2478a5a28fb
Luke Godfrey <thelukester92@me.com>
2017-05-18 15:37:53 -0500

    Removed NNWarn.

--- include/nnlib/util/error.h
+++ include/nnlib/util/error.h
@@ -11,18 +11,6 @@
 namespace nnlib
 {
 
-/// Warnings that act as asserts in DEBUG mode.
-#define NNWarn(x, m)											\
-{																\
-	if(!(x))													\
-	{															\
-		std::cerr												\
-			<< "Warning in " << __func__						\
-			<< " at " __FILE__ << ":" << __LINE__ << std::endl	\
-			<< "\t" << m << std::endl;							\
-	}															\
-}
-
 /// Asserts that should never be optimized out.
 #define NNHardAssert(x, m)										\
 {																\
@@ -42,18 +30,6 @@ namespace nnlib
 	#define NNAssert(x, m) NNHardAssert(x, m)
 #else
 	#define NNAssert(x, m) (void) 0
-#endif
-
-/// Turn warns into asserts in debug mode.
-#ifdef DEBUG
-	#undef NNWarn
-	#define NNWarn(x, m) NNHardAssert(x, m)
-#endif
-
-/// Turn warns into noops in optimize mode.
-#ifdef OPTIMIZE
-	#undef NNWarn
-	#define NNWarn(x, m) (void) 0
 #endif
 
 }
