d1d91e2270a8027c5c94f1e9d076c1928e8478ab
Luke Godfrey <thelukester92@me.com>
2017-06-12 13:48:29 -0500

    Removed the assert, it was wrong.

--- include/nnlib/tensor.h
+++ include/nnlib/tensor.h
@@ -437,7 +437,6 @@ public:
 		size_t dim = 0;
 		for(const std::initializer_list<size_t> &params : dims)
 		{
-			NNAssertGreaterThan(params.size(), 0, "Invalid parameters for subtensor!");
 			NNAssertLessThanOrEquals(params.size(), 2, "Invalid parameters for subtensor!");
 			if(params.size() == 1)
 			{
