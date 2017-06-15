7e8969e2335b5e86ba4b8c350b9fe4d2ce80a107
Luke Godfrey <thelukester92@me.com>
2017-06-15 11:22:56 -0500

    Better error test.

--- test/test_error.h
+++ test/test_error.h
@@ -8,12 +8,14 @@ using namespace nnlib;
 void TestError()
 {
 	Tensor<> tensor(3, 6, 9);
+	
 	Error e("file", "func", 123, tensor(0, 1, 2), tensor, std::string("failure"));
-	try
-	{
-		throw e;
-	}
-	catch(Error &e) {}
+	try { throw e; }
+	catch(const Error &) {}
+	
+	Error f("this is an error message");
+	try { throw f; }
+	catch(const Error &) {}
 }
 
 #endif
