2bcee0eb7eceade66dcfe8e3f292b6513a387319
Luke Godfrey <thelukester92@me.com>
2017-10-20 12:49:23 -0500

    Fixed LSTM!.

--- test/nn/test_sequencer.hpp
+++ test/nn/test_sequencer.hpp
@@ -9,97 +9,45 @@ using namespace nnlib;
 void TestSequencer()
 {
 	// Input, arbitrary
-	Tensor<> inp = Tensor<>({
-		-5, 10,
-		15, -20,
-		3, 4
-	}).resize(3, 1, 2);
+	Tensor<> inp = Tensor<>({ 8, 6, 0 }).resize(3, 1, 1);
 	
 	// Output gradient, arbitrary
-	Tensor<> grd = Tensor<>({
-		1, 2, 3,
-		3, -4, 5,
-		-3, 2, -7
-	}).resize(3, 1, 3);
+	Tensor<> grd = Tensor<>({ 1, 0, -1 }).resize(3, 1, 1);
 	
 	// LSTM layer with specific weights and bias, arbitrary
-	LSTM<> *lstm = new LSTM<>(2, 3);
+	LSTM<> *lstm = new LSTM<>(1, 1);
 	lstm->params().copy({
-		// inpGateX: 9
-		0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 1.0, 0.9, 1.7,
+		// inpGate (x, y, h, b)
+		-0.2, 0.5, 0.1, 0.0,
 		
-		// inpGateY: 9
-		1, 0, 1, 2, 1, 0, 1, 2, 3,
+		// fgtGate (x, y, h, b)
+		0.75, -0.6, 0.25, 0.0,
 		
-		// inpGateH: 9
-		2, 1, 2, 1, 2, 1, 2, 1, 2,
+		// inpMod (x, y, b)
+		1.0, -0.7, 0.0,
 		
-		// fgtGateX: 9
-		0.5, 0.4, 0.5, 0.3, 0.2, -0.1, 15.7, 8.8, 14.3,
-		
-		// fgtGateY: 9
-		9, 8, 7, 6, 5, 6, 7, 8, 9,
-		
-		// fgtGateH: 9
-		1, 2, 1, 4, 1, 5, 1, 6, 1,
-		
-		// inpModX: 9
-		8, 6, 7, 5, 3, 0, 14, 6, 7,
-		
-		// inpModY: 9
-		5, 5, 5, 5, 5, 5, 5, 5, 5,
-		
-		// outGateX: 9
-		0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 1.1, 2.7, 1.875,
-		
-		// outGateY: 9
-		1, 2, 3, 2, 1, 2, 3, 2, 1,
-		
-		// outGateH: 9
-		0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9
+		// outGate (x, y, h', b)
+		0.3, 0.3, -0.75, 0.0
 	});
 	
 	// Output, fixed given input and weights
-	Tensor<> out = Tensor<>({
-		0.69628, 0.73962, -0.69862,
-		0.75505, 0.76076, -0.75973,
-		0.96590, 0.96347, -0.00642
-	}).resize(3, 1, 3);
+	Tensor<> out = Tensor<>({ 0.15089258930, 0.32260369939, 0.03848645247 }).resize(3, 1, 1);
 	
 	// Reversed output, fixed given input and weights
-	Tensor<> rOut = Tensor<>({
-		0.99347, 0.98614, 0.70684,
-		0.95272, 0.90196, 0.95457,
-		0.73827, 0.74884, 0.75822
-	}).resize(3, 1, 3);
+	Tensor<> rOut = Tensor<>({ 0.3523252224669841, 0.1900534836059371, 0 }).resize(3, 1, 1);
 	
 	// Input gradient, fixed given input and output gradient
-	Tensor<> ing = Tensor<>({
-		-0.03112, -0.00071,
-		0.04170, 0.02960,
-		-0.02195, -0.04333
-	}).resize(3, 1, 2);
+	Tensor<> ing = Tensor<>({ -0.01712796895, 0.00743178473, -0.30729831287 }).resize(3, 1, 1);
 	
 	// Reversed input gradient, fixed given input and output gradient
-	Tensor<> rIng = Tensor<>({
-		0.00002, 0.00001,
-		0.02605, 0.03943,
-		-0.00803, -0.01839
-	}).resize(3, 1, 2);
+	Tensor<> rIng = Tensor<>({ -0.01058507197696686, -0.02743768942521932, 0.1646924317207443 }).resize(3, 1, 1);
 	
 	// Parameter gradient, fixed given the input and output gradient
 	Tensor<> prg = Tensor<>({
-		0.47546, -0.17090, -0.40531, -0.59076,  0.22103, -0.01223,  0.05094, -0.01696, -0.07484,
-		0.02503, -0.00834, -0.06154,  0.02662, -0.00891, -0.06239, -0.02511,  0.00836,  0.06190,
-		0.03558, -0.01189, -0.08465,  0.03574, -0.01198, -0.08228, -0.03595,  0.01205,  0.08198,
-		0.00000, -0.00025,  0.00000,  0.00000,  0.00034,  0.00000,  0.00000, -0.00002,  0.00000,
-		0.00000, -0.00001,  0.00000,  0.00000, -0.00001,  0.00000,  0.00000,  0.00001,  0.00000,
-		0.00000, -0.00002,  0.00000,  0.00000, -0.00002,  0.00000,  0.00000,  0.00002,  0.00000,
-		0.00000,  0.00008,  0.00000,  0.00000, -0.00016,  0.00000,  0.00000, -0.00002,  0.00000,
-		0.00000,  0.00000,  0.00000,  0.00000,  0.00000,  0.00000,  0.00000,  0.00000,  0.00000,
-		0.58074, -0.27490,  0.91818, -0.57242,  0.50183, -1.84492,  0.12095,  0.03566, -0.18707,
-		0.04098, -0.00334, -0.00060,  0.04355, -0.00355, -0.00063, -0.04112,  0.00335,  0.00060,
-		0.00000,  0.00000,  0.00000,  0.00000,  0.00000,  0.00000,  0.00000,  0.00000,  0.00000
+		 0.73850659626,  0.00585685005,  0.00801518653,  0.11462669318,
+		-0.00117516696, -0.01646944995, -0.02114722491, -0.05115589662,
+		-0.00000416626, -0.08323296026, -0.25800409062,
+		 0.18717939172, -0.00419251188,  0.00611825134,  0.00767284875
 	});
 	
 	// Test forward and backward using the parameters and targets above
@@ -118,6 +66,8 @@ void TestSequencer()
 	module.reverse(true);
 	module.forward(inp);
 	module.backward(inp, grd);
+	
+	std::cout << "got:\n" << module.output().select(1, 0) << std::endl << module.inGrad().select(1, 0) << std::endl;
 	
 	NNAssertLessThan(module.output().add(rOut, -1).square().sum(), 1e-9, "Sequencer::forward (reversed) failed!");
 	NNAssertLessThan(module.inGrad().add(rIng, -1).square().sum(), 1e-9, "Sequencer::backward (reversed) failed; wrong inGrad!");
