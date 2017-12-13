7291088e3acb4cdf49189186cd62e41dee8fbe91
Luke Godfrey <thelukester92@me.com>
2017-12-13 17:30:00 -0600

    Split Sequential.

--- include/nnlib/nn/sequential.hpp
+++ include/nnlib/nn/sequential.hpp
@@ -14,33 +14,10 @@ public:
 	using Container<T>::Container;
 	using Container<T>::components;
 	
-	/// Forward propagate input, returning output.
-	virtual Tensor<T> &forward(const Tensor<T> &input) override
-	{
-		Tensor<T> *inp = const_cast<Tensor<T> *>(&input);
-		for(Module<T> *component : m_components)
-			inp = &component->forward(*inp);
-		return *inp;
-	}
-	
-	/// Backward propagate input and output gradient, returning input gradient.
-	virtual Tensor<T> &backward(const Tensor<T> &input, const Tensor<T> &outGrad) override
-	{
-		const Tensor<T> *grad = &outGrad;
-		for(size_t i = components() - 1; i > 0; --i)
-			grad = &m_components[i]->backward(m_components[i - 1]->output(), *grad);
-		return m_components[0]->backward(input, *grad);
-	}
-	
-	virtual Tensor<T> &output() override
-	{
-		return m_components.back()->output();
-	}
-	
-	virtual Tensor<T> &inGrad() override
-	{
-		return m_components[0]->inGrad();
-	}
+	virtual Tensor<T> &forward(const Tensor<T> &input) override;
+	virtual Tensor<T> &backward(const Tensor<T> &input, const Tensor<T> &outGrad) override;
+	virtual Tensor<T> &output() override;
+	virtual Tensor<T> &inGrad() override;
 	
 protected:
 	using Container<T>::m_components;
@@ -49,5 +26,6 @@ protected:
 }
 
 NNRegisterType(Sequential, Module);
+NNTemplateDefinition(Sequential, "detail/sequential.tpp");
 
 #endif
---
+++ src/nn/sequential.cpp
@@ -0,0 +1,8 @@
+#ifdef NN_REAL_T
+
+#include "nnlib/nn/sequential.hpp"
+#include "nnlib/nn/detail/sequential.tpp"
+
+template class nnlib::Sequential<NN_REAL_T>;
+
+#endif
