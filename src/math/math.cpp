4a8d74b5263d6868d372eeb59c1319f1dd964446
Luke Godfrey <thelukester92@me.com>
2017-12-14 16:11:07 -0600

    Added nvblas.

--- src/math/math.cpp
+++ src/math/math.cpp
@@ -4,6 +4,7 @@
 #include "nnlib/math/math.hpp"
 #include "nnlib/math/detail/math.tpp"
 #include "nnlib/math/detail/blas.tpp"
+#include "nnlib/math/detail/nvblas.tpp"
 
 template class nnlib::Math<NN_REAL_T>;
 
