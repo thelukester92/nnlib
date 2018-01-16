b04b3a831290dcc003ed471fc274fdf942341c18
Luke Godfrey <thelukester92@me.com>
2018-01-16 11:59:30 -0600

    More Storage tests.

--- test/core/detail/test_storage.cpp
+++ test/core/detail/test_storage.cpp
@@ -77,4 +77,89 @@ NNTestClassImpl(Storage)
                 NNTestEquals(s[i], i);
         }
     }
+
+    NNTestMethod(resize)
+    {
+        NNTestParams(size_t, const T &)
+        {
+            Storage<int> s;
+            s.resize(5, 42);
+            NNTestEquals(s.size(), 5);
+            for(size_t i = 0; i < 5; ++i)
+                NNTestEquals(s[i], 42);
+        }
+    }
+
+    NNTestMethod(reserve)
+    {
+        NNTestParams(size_t)
+        {
+            Storage<int> s;
+            int *a, *b, *c;
+            s.reserve(2);
+            a = s.ptr();
+            s.push_back(0);
+            s.push_back(0);
+            b = s.ptr();
+            s.push_back(0);
+            c = s.ptr();
+            NNTestEquals(s.size(), 3);
+            NNTestEquals(a, b);
+            NNTestNotEquals(b, c);
+        }
+    }
+
+    NNTestMethod(push_back)
+    {
+        NNTestParams(const T &)
+        {
+            Storage<int> s;
+            NNTestEquals(s.push_back(42), s);
+            NNTestEquals(s.size(), 1);
+            NNTestEquals(s[0], 42);
+        }
+    }
+
+    NNTestMethod(pop_back)
+    {
+        NNTestParams(const T &)
+        {
+            Storage<int> s(5, 42);
+            NNTestEquals(s, s.pop_back());
+            NNTestEquals(s.size(), 4);
+        }
+    }
+
+    NNTestMethod(append)
+    {
+        NNTestParams(const Storage &)
+        {
+            Storage<int> s(2, 42);
+            Storage<int> t(3, -3);
+            NNTestEquals(s.append(t), s);
+            NNTestEquals(s.size(), 5);
+            NNTestEquals(s[0], 42);
+            NNTestEquals(s[1], 42);
+            NNTestEquals(s[2], -3);
+            NNTestEquals(s[3], -3);
+            NNTestEquals(s[4], -3);
+        }
+    }
+
+    NNTestMethod(erase)
+    {
+        NNTestParams(size_t)
+        {
+            Storage<int> s({ 0, 1, 2, 3, 4, 5 });
+            NNTestEquals(s.erase(0), s);
+            NNTestEquals(s.size(), 5);
+            NNTestEquals(s[0], 1);
+            s.erase(1);
+            NNTestEquals(s.size(), 4);
+            NNTestEquals(s[1], 3);
+            s.erase(3);
+            NNTestEquals(s.size(), 3);
+            NNTestEquals(s[2], 4);
+        }
+    }
 }
