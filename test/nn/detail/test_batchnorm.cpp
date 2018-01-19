8aecb8d3be28209681537621b15283df47a82392
Luke Godfrey <thelukester92@me.com>
2018-01-18 18:31:47 -0600

    Finished BatchNorm tests.

--- test/nn/detail/test_batchnorm.cpp
+++ test/nn/detail/test_batchnorm.cpp
@@ -145,7 +145,45 @@ NNTestClassImpl(BatchNorm)
     {
         NNTestParams(const Tensor &, const Tensor &)
         {
+            auto input = Tensor<T>({ 3, 6, 9, -1, 5, 4, 12, 5, 11 }).resize(3, 3);
+            auto blame = Tensor<T>({ 2, 3, 4, -2, 0, 4, 10, 2, 4 }).resize(3, 3);
+            auto inGrad = Tensor<T>({ 0.03596, 0, 0, -0.02489, -2.12132, 0, -0.01106, 2.12132, 0 }).resize(3, 3);
+            auto pGrad = Tensor<T>({ 14.9606, 2.82843, 0, 10, 5, 12 });
 
+            BatchNorm<T> module(3);
+            module.weights().ones();
+            module.bias().zeros();
+            module.momentum(1.0);
+
+            module.forward(input);
+            module.backward(input, blame);
+
+            forEach([&](T actual, T target)
+            {
+                NNTestAlmostEquals(actual, target, 1e-3);
+            }, module.inGrad(), inGrad);
+
+            forEach([&](T actual, T target)
+            {
+                NNTestAlmostEquals(actual, target, 1e-3);
+            }, module.grad(), pGrad);
+
+            module.training(false);
+            inGrad.copy({ 0.30038, 5.19615, 1.10940, -0.30038, 0, 1.10940, 1.50188, 3.46410, 1.10940 });
+            pGrad.copy({ 27.17588, 5.13783, 0, 20, 10, 24 });
+
+            module.forward(input);
+            module.backward(input, blame);
+
+            forEach([&](T actual, T target)
+            {
+                NNTestAlmostEquals(actual, target, 1e-3);
+            }, module.inGrad(), inGrad);
+
+            forEach([&](T actual, T target)
+            {
+                NNTestAlmostEquals(actual, target, 1e-3);
+            }, module.grad(), pGrad);
         }
     }
 
@@ -153,100 +191,18 @@ NNTestClassImpl(BatchNorm)
     {
         NNTestParams()
         {
-            // weights, biases
-        }
-    }
+            BatchNorm<T> module(10);
+            module.params().fill(3.14);
 
-    NNTestMethod(gradList)
-    {
-        NNTestParams()
-        {
-            // weightsGrad, biasesGrad
-        }
-    }
+            forEach([&](T param)
+            {
+                NNTestAlmostEquals(param, 3.14, 1e-12);
+            }, module.weights());
 
-    NNTestMethod(stateList)
-    {
-        NNTestParams()
-        {
-            // means, invStds, runningMeans, runningVars
+            forEach([&](T param)
+            {
+                NNTestAlmostEquals(param, 3.14, 1e-12);
+            }, module.bias());
         }
     }
 }
-
-/*
-void TestBatchNorm()
-{
-    Tensor<NN_REAL_T> inp = Tensor<NN_REAL_T>({
-         3,  6,  9,
-        -1,  5,  4,
-        12,  5, 11
-    }).resize(3, 3);
-
-    Tensor<NN_REAL_T> grad = Tensor<NN_REAL_T>({
-         2,  3,  4,
-        -2,  0,  4,
-        10,  2,  4
-    }).resize(3, 3);
-
-    Tensor<NN_REAL_T> inGrad = Tensor<NN_REAL_T>({
-         0.03596,  0.00000,  0,
-        -0.02489, -2.12132,  0,
-        -0.01106,  2.12132,  0
-    }).resize(3, 3);
-
-    Tensor<NN_REAL_T> paramGrad = Tensor<NN_REAL_T>({
-        14.9606, 2.82843, 0,
-        10, 5, 12
-    });
-
-    BatchNorm<NN_REAL_T> bn(3);
-    bn.weights().ones();
-    bn.bias().zeros();
-    bn.momentum(1.0);
-
-    bn.forward(inp);
-    for(size_t i = 0; i < 3; ++i)
-    {
-        NNAssertLessThan(fabs(math::mean(bn.output().select(1, i))), 1e-9, "BatchNorm::forward failed! Non-zero mean!");
-        NNAssertAlmostEquals(math::variance(bn.output().select(1, i)), 1, 1e-9, "BatchNorm::forward failed! Non-unit variance!");
-    }
-
-    bn.backward(inp, grad);
-    NNAssertLessThan(math::sum(math::square(bn.grad() - paramGrad)), 1e-9, "BatchNorm::backward failed! Wrong parameter gradient!");
-    NNAssertLessThan(math::sum(math::square(bn.inGrad() - inGrad)), 1e-9, "BatchNorm::backward failed! Wrong input gradient!");
-
-    bn.training(false);
-
-    paramGrad.copy({
-        27.17588, 5.13783, 0,
-        20, 10, 24
-    });
-
-    inGrad.copy({
-         0.30038, 5.19615, 1.10940,
-        -0.30038, 0.00000, 1.10940,
-         1.50188, 3.46410, 1.10940
-    });
-
-    bn.forward(inp);
-    for(size_t i = 0; i < 3; ++i)
-    {
-        NNAssertLessThan(fabs(math::mean(bn.output().select(1, i))), 1e-9, "BatchNorm::forward (inference) failed! Non-zero mean!");
-        NNAssertAlmostEquals(
-            math::variance(bn.output().select(1, i).scale(sqrt(3) / sqrt(2))), 1, 1e-9,
-            "BatchNorm::forward (inference) failed! Non-unit variance!"
-        );
-    }
-
-    bn.backward(inp, grad);
-    NNAssertLessThan(math::sum(math::square(bn.grad() - paramGrad)), 1e-9, "BatchNorm::backward (inference) failed! Wrong parameter gradient!");
-    NNAssertLessThan(math::sum(math::square(bn.inGrad() - inGrad)), 1e-9, "BatchNorm::backward (inference) failed! Wrong input gradient!");
-
-    Tensor<NN_REAL_T> state = bn.state();
-    state.fill(0);
-    NNAssertAlmostEquals(math::sum(bn.output()), 0, 1e-12, "BatchNorm::state failed!");
-
-    TestModule("BatchNorm", bn, inp);
-}
-*/
