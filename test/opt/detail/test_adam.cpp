3c7780798af6590c8c11436dbcb105e3f533b0fe
Luke Godfrey <thelukester92@me.com>
2018-01-19 17:02:40 -0600

    Finished Adam tests.

--- test/opt/detail/test_adam.cpp
+++ test/opt/detail/test_adam.cpp
@@ -16,65 +16,87 @@ NNTestClassImpl(Adam)
 
     NNTestMethod(reset)
     {
-        RandomEngine::sharedEngine().seed(0);
-
-        Linear<T> model(2, 3);
-        MSE<T> critic;
-        Adam<T> opt(model, critic);
-
-        auto inputs = math::rand(Tensor<T>(2));
-        auto target = math::rand(Tensor<T>(3));
-
-        auto before = model.params().copy();
-        for(size_t i = 0; i < 100; ++i)
-            opt.step(inputs, target);
-        auto after = model.params().copy();
-
-        model.params().copy(before);
-        opt.reset();
-        for(size_t i = 0; i < 100; ++i)
-            opt.step(inputs, target);
-
-        forEach([&](T first, T second)
+        NNTestParams()
         {
-            NNTestAlmostEquals(first, second, 1e-12);
-        }, after, model.params());
+            RandomEngine::sharedEngine().seed(0);
+
+            Linear<T> model(2, 3);
+            MSE<T> critic;
+            Adam<T> opt(model, critic);
+
+            auto inputs = math::rand(Tensor<T>(2));
+            auto target = math::rand(Tensor<T>(3));
+
+            auto before = model.params().copy();
+            for(size_t i = 0; i < 100; ++i)
+                opt.step(inputs, target);
+            auto after = model.params().copy();
+
+            model.params().copy(before);
+            opt.reset();
+            for(size_t i = 0; i < 100; ++i)
+                opt.step(inputs, target);
+
+            forEach([&](T first, T second)
+            {
+                NNTestAlmostEquals(first, second, 1e-12);
+            }, after, model.params());
+        }
     }
 
     NNTestMethod(learningRate)
     {
-        RandomEngine::sharedEngine().seed(0);
+        NNTestParams(T)
+        {
+            RandomEngine::sharedEngine().seed(0);
 
-        Linear<T> model(2, 3);
-        MSE<T> critic;
-        Adam<T> opt(model, critic);
+            Linear<T> model(2, 3);
+            MSE<T> critic;
+            Adam<T> opt(model, critic);
 
-        auto inputs = math::rand(Tensor<T>(2));
-        auto target = math::rand(Tensor<T>(3));
+            auto inputs = math::rand(Tensor<T>(2));
+            auto target = math::rand(Tensor<T>(3));
 
-        opt.learningRate(0.1);
-        NNTestAlmostEquals(opt.learningRate(), 0.1, 1e-12);
+            opt.learningRate(0.1);
+            NNTestAlmostEquals(opt.learningRate(), 0.1, 1e-12);
 
-        auto before = model.params().copy();
-        opt.step(inputs, target);
-        T err1 = critic.forward(model.forward(inputs), target);
+            auto before = model.params().copy();
+            opt.step(inputs, target);
+            T dist1 = sqrt(math::sum(math::square(before - model.params())));
 
-        model.params().copy(before);
-        opt.reset();
-        opt.learningRate(1);
-        opt.step(inputs, target);
-        T err2 = critic.forward(model.forward(inputs), target);
+            model.params().copy(before);
+            opt.reset();
+            opt.learningRate(1);
+            opt.step(inputs, target);
+            T dist2 = sqrt(math::sum(math::square(before - model.params())));
 
-        NNTestLessThan(err2, err1);
+            NNTestAlmostEquals(dist1, 0.1 * dist2, 1e-12);
+        }
     }
 
     NNTestMethod(beta1)
     {
-
+        NNTestParams(T)
+        {
+            Linear<T> model(1, 1);
+            model.params().fill(1);
+            MSE<T> critic;
+            Adam<T> opt(model, critic);
+            opt.beta1(0.5);
+            NNTestAlmostEquals(opt.beta1(), 0.5, 1e-12);
+        }
     }
 
     NNTestMethod(beta2)
     {
-
+        NNTestParams(T)
+        {
+            Linear<T> model(1, 1);
+            model.params().fill(1);
+            MSE<T> critic;
+            Adam<T> opt(model, critic);
+            opt.beta2(0.5);
+            NNTestAlmostEquals(opt.beta2(), 0.5, 1e-12);
+        }
     }
 }
