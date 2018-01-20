75302139afff2541019dedc884c8cfc71a1a4ecd
Luke Godfrey <thelukester92@me.com>
2018-01-20 16:26:27 -0600

    Added FileSerializer::write tests.

--- test/serialization/detail/test_fileserializer.cpp
+++ test/serialization/detail/test_fileserializer.cpp
@@ -66,7 +66,7 @@ NNTestClassImpl(FileSerializer)
             JSONSerializer::write(s, ".nnlib.json");
             try
             {
-                Serialized t = JSONSerializer::read(".nnlib.json");
+                Serialized t = FileSerializer::read(".nnlib.json");
                 NNTestEquals(s.size(), 2);
                 NNTestEquals(s.size(0), 3);
                 NNTestEquals(s.size(1), 2);
@@ -89,7 +89,74 @@ NNTestClassImpl(FileSerializer)
     {
         NNTestParams(const Serialized &, const std::string &)
         {
+            Serialized s;
+            s.add(Serialized::Array);
+            s.get(0)->add(0);
+            s.get(0)->add(3.14);
+            s.get(0)->add("string");
+            s.add(Serialized::Array);
+            s.get(1)->add("a,string");
+            s.get(1)->add(-2);
 
+            FileSerializer::write(s, ".nnlib.bin");
+            try
+            {
+                Serialized t = BinarySerializer::read(".nnlib.bin");
+                NNTestEquals(s.size(), 2);
+                NNTestEquals(s.size(0), 3);
+                NNTestEquals(s.size(1), 2);
+                NNTestEquals(s.get(0)->get<int>(0), 0);
+                NNTestEquals(s.get(0)->get<double>(1), 3.14);
+                NNTestEquals(s.get(0)->get<std::string>(2), "string");
+                NNTestEquals(s.get(1)->get<std::string>(0), "a,string");
+                NNTestEquals(s.get(1)->get<int>(1), -2);
+            }
+            catch(const Error &e)
+            {
+                remove(".nnlib.bin");
+                throw e;
+            }
+            remove(".nnlib.bin");
+
+            FileSerializer::write(s, ".nnlib.csv");
+            try
+            {
+                Serialized t = CSVSerializer::read(".nnlib.csv");
+                NNTestEquals(s.size(), 2);
+                NNTestEquals(s.size(0), 3);
+                NNTestEquals(s.size(1), 2);
+                NNTestEquals(s.get(0)->get<int>(0), 0);
+                NNTestEquals(s.get(0)->get<double>(1), 3.14);
+                NNTestEquals(s.get(0)->get<std::string>(2), "string");
+                NNTestEquals(s.get(1)->get<std::string>(0), "a,string");
+                NNTestEquals(s.get(1)->get<int>(1), -2);
+            }
+            catch(const Error &e)
+            {
+                remove(".nnlib.csv");
+                throw e;
+            }
+            remove(".nnlib.csv");
+
+            FileSerializer::write(s, ".nnlib.json");
+            try
+            {
+                Serialized t = JSONSerializer::read(".nnlib.json");
+                NNTestEquals(s.size(), 2);
+                NNTestEquals(s.size(0), 3);
+                NNTestEquals(s.size(1), 2);
+                NNTestEquals(s.get(0)->get<int>(0), 0);
+                NNTestEquals(s.get(0)->get<double>(1), 3.14);
+                NNTestEquals(s.get(0)->get<std::string>(2), "string");
+                NNTestEquals(s.get(1)->get<std::string>(0), "a,string");
+                NNTestEquals(s.get(1)->get<int>(1), -2);
+            }
+            catch(const Error &e)
+            {
+                remove(".nnlib.json");
+                throw e;
+            }
+            remove(".nnlib.json");
         }
     }
 }
