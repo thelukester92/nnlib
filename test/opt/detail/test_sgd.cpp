61901d729d52d5fafa2a10386ff3bb7fe278d243
Luke Godfrey <thelukester92@me.com>
2018-01-20 12:20:37 -0600

    Better SGD test.

--- test/opt/detail/test_sgd.cpp
+++ test/opt/detail/test_sgd.cpp
@@ -16,11 +16,44 @@ NNTestClassImpl(SGD)
     {
         NNTestParams(T)
         {
-            Linear<T> model(1, 1);
-            model.params().fill(1);
+            Linear<T> model(1, 1, false);
+            model.params().copy({ 1 });
+
             SGD<T> opt(model);
+            opt.learningRate(0.25);
             opt.momentum(0.5);
             NNTestAlmostEquals(opt.momentum(), 0.5, 1e-12);
+
+            Tensor<T> inputs = { 1 };
+            Tensor<T> target = { 0 };
+
+            opt.step(inputs, target);
+            NNTestAlmostEquals(opt.params()(0), 0.25, 1e-12);
+
+            opt.step(inputs, target);
+            NNTestAlmostEquals(opt.params()(0), -0.0625, 1e-12);
+        }
+    }
+
+    NNTestMethod(step)
+    {
+        NNTestParams(const Tensor &, const Tensor &)
+        {
+            Linear<T> model(1, 1, false);
+            model.params().copy({ 1 });
+
+            SGD<T> opt(model);
+            opt.learningRate(0.25);
+            opt.momentum(0);
+
+            Tensor<T> inputs = { 1 };
+            Tensor<T> target = { 0 };
+
+            opt.step(inputs, target);
+            NNTestAlmostEquals(opt.params()(0), 0.5, 1e-12);
+
+            opt.step(inputs, target);
+            NNTestAlmostEquals(opt.params()(0), 0.25, 1e-12);
         }
     }
 }
