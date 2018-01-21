e56e2f1e1ec2d39ec7f3a2b9cbe908b5c298d7b7
Luke Godfrey <thelukester92@me.com>
2018-01-20 22:07:57 -0600

    Split timer into hpp/tpp/cpp triple.

--- include/nnlib/util/timer.hpp
+++ include/nnlib/util/timer.hpp
@@ -2,7 +2,6 @@
 #define UTIL_TIMER_HPP
 
 #include <chrono>
-#include <iomanip>
 #include <sstream>
 
 namespace nnlib
@@ -10,57 +9,22 @@ namespace nnlib
 
 class Timer
 {
-using clock = std::chrono::high_resolution_clock;
 public:
-    Timer(std::chrono::time_point<clock> start = clock::now())
-        : m_start(start)
-    {}
+    using clock = std::chrono::high_resolution_clock;
 
-    void reset()
-    {
-        m_start = clock::now();
-    }
-
-    double elapsed(bool startOver = false)
-    {
-        double span = std::chrono::duration<double>(clock::now() - m_start).count();
-        if(startOver)
-            reset();
-        return span;
-    }
-
-    std::string ftime(double t = -1)
-    {
-        if(t < 0)
-            t = elapsed();
-
-        std::ostringstream out;
-        out << std::setprecision(1) << std::fixed;
-
-        size_t m = t / 60;
-        t -= m * 60;
-
-        size_t h = m / 60;
-        m -= h * 60;
-
-        size_t d = h / 24;
-        h -= d * 24;
-
-        if(d > 0)
-            out << d << "d ";
-        if(d > 0 || h > 0)
-            out << h << "h ";
-        if(d > 0 || h > 0 || m > 0)
-            out << m << "m ";
-        out << t << "s";
-
-        return out.str();
-    }
+    Timer(std::chrono::time_point<clock> start = clock::now());
+    void reset();
+    double elapsed(bool startOver = false);
+    std::string ftime(double t = -1);
 
 private:
     std::chrono::time_point<clock> m_start;
 };
 
 }
+
+#if !defined NN_REAL_T && !defined NN_IMPL
+    #include "detail/timer.tpp"
+#endif
 
 #endif
